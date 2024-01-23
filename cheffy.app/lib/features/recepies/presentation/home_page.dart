5cb6720a80d675f15d60ed7f21f3b4358115365f
Serge Najimov <najimovserge@gmail.com>
2024-01-23 11:01:20 +0100

    temporary solution for idToken

--- cheffy.app/lib/features/recepies/presentation/home_page.dart
+++ cheffy.app/lib/features/recepies/presentation/home_page.dart
@@ -1,12 +1,33 @@
 import 'package:cheffy/common/components/auth_button.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String token = "";
 
   void logout() async {
     await FirebaseAuth.instance.signOut();
+  }
+
+  _httpCall() async {
+    final user = FirebaseAuth.instance.currentUser;
+    final idToken = await user!.getIdToken();
+    setState(() {
+      if (idToken == null) {
+        token = "not found";
+      } else {
+        token = idToken;
+        Clipboard.setData(ClipboardData(text: token));
+      }
+    });
   }
 
   @override
@@ -21,7 +42,10 @@ class HomePage extends StatelessWidget {
             children: [
               const Text("Home Page"),
               const SizedBox(height: 20),
-              AuthButton(onTap: logout, text: "Logout")
+              AuthButton(onTap: logout, text: "Logout"),
+              const SizedBox(height: 20),
+              AuthButton(onTap: _httpCall, text: "Get token"),
+              Text(token)
             ],
           ),
         ),
